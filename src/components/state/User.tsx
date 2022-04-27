6949cd56c1ff05c727337be2641768f380effdd6
Denis Listiadi <denislistiadi24@gmail.com>
2022-04-27 14:00:05 +0700

    useState Type Assertion

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -6,23 +6,19 @@ type AuthUserProps = {
 };
 
 const User = () => {
-  const [user, setUser] = useState<AuthUserProps | null>(null);
+  const [user, setUser] = useState<AuthUserProps>({} as AuthUserProps);
   const handleLogin = () => {
     setUser({
       name: "Denis",
       email: "denislistiadi@gmail.com",
     });
   };
-  const handleLogout = () => {
-    setUser(null);
-  };
 
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
-      <div>User name is {user?.name}</div>
-      <div>User email is {user?.email} </div>
+      <div>User name is {user.name}</div>
+      <div>User email is {user.email} </div>
     </div>
   );
 };
